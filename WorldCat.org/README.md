5d4b9ae4ec9ef882cf390c87267ae66b58a3c082
Michael J. Kazin <mkazin@gmail.com>
2023-04-25 20:41:20 -0400

    Added screenshots to WorldCat

--- WorldCat.org/README.md
+++ WorldCat.org/README.md
@@ -7,4 +7,8 @@
 
 Adds a user interface to add Items to one of your personal Lists quickly from the search page (coming soon: from the Title page too).
 
+|Turns this | Into this |
+|---|---|
+|<img src="img/WorldCat-FastAdd-1Before.png"><br><img src="img/WorldCat-FastAdd-1BeforeClickTwoThree.png">|<img src="img/WorldCat-FastAdd-2After.png">|
+
 
--- WorldCat.org/WorldCat Fast-Add-to-List.user.js
+++ WorldCat.org/WorldCat Fast-Add-to-List.user.js
@@ -15,7 +15,7 @@
 const LISTS_REQUEST_URL="https://www.worldcat.org/api/lists?limit=100&offset=1&orderBy=name-asc&listName=&isInstitutionalUser=false"
 
 const ADD_BUTTON_CONTAINER_CLASS = ".MuiButton-startIcon.MuiButton-iconSizeMedium"
-const ITEM_TITLE_CLASS = 'div h2 a'
+const ITEM_TITLE_CLASS = 'div h2 a.tss-1cxpzkn-root'
 const ITEM_CONTAINER_CLASS = '.MuiGrid-item'
 const SEARCH_CHECK_INTERVAL = 2500
 
@@ -33,22 +33,25 @@ function queryLists() {
 
 function buildExistingListsContainers() {
     Array.from(document.querySelectorAll(ADD_BUTTON_CONTAINER_CLASS)).forEach(container => {
-
+console.log("B")
+console.log(container.closest(ITEM_CONTAINER_CLASS).querySelectorAll(ITEM_TITLE_CLASS)[0])
         const itemId = container.closest(ITEM_CONTAINER_CLASS).querySelector(ITEM_TITLE_CLASS).href.split('/').at(-1)
         let listContainer = document.createElement("div")
         let containerLabel = document.createElement("span")
         containerLabel.innerText = "Quick Add to List: "
         listContainer.append(containerLabel)
+        let unorderedList = document.createElement("ul")
+        listContainer.append(unorderedList)
         lists.forEach( (listName, listId) => {
 
-            let listLink = document.createElement("span")
+            let listLink = document.createElement("li")
             listLink.innerText = listName
             listLink.style.color = 'blue'
             listLink.onclick = () => {
                 const post_url = `https://www.worldcat.org/api/lists/${listId}?listId=${listId}&itemId=${itemId}&isInstitutionalUser=false`
                 fetch(post_url, { method: 'POST' }).then((response) => console.log(response))
             }
-            listContainer.append(listLink)
+            unorderedList.append(listLink)
         })
         container.closest(".MuiBox-root").append(listContainer)
     })
@@ -61,6 +64,8 @@ function startSearchInterval() {
 }
 function checkSearchCompletion() {
     if (document.querySelectorAll(ITEM_TITLE_CLASS).length > 0) {
+console.log("A")
+console.log(document.querySelectorAll(ITEM_TITLE_CLASS)[0])
         clearInterval(search_interval)
         onSearchCompletion()
     }
