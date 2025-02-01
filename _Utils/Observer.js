1e81134be656ddb5e4f067313e15c16402244ee2
Michael J. Kazin <mkazin@gmail.com>
2025-02-01 17:08:09 -0500

    Observer 0.0.3 - ShadowRoot support (partial)
    Refactorered the code "a bit"
    Disabled unit test for bug described in #10

--- _Utils/Observer.js
+++ _Utils/Observer.js
@@ -1,5 +1,5 @@
 /**
- * whenElementAppears() - A reusable, single-line MutationObserver utility function
+ * ObserverTracker - A reusable, single-line MutationObserver utility class
  *
  * Sets up a mutation observer to watch for the *first* appearance of a specified selector in the DOM.
  *
@@ -11,71 +11,7 @@
  * @param {function(Node)} fn - Callback function to invoke when the selector is observed.
  * @param {Element} [observationTarget=document] - Optional node to observe for the appearance of the selector. If not provided, the entire DOM will be observed.
  * @param {Number} [timeout] - Optional timeout in ms to disable observer. If set to 0, mutationobserver will continue to run until the selector is seen.
- * @returns {void}
 */
-function whenElementAppears(selector, fn, observationTarget = document, timeout = 0) {
-    const tracker = new ObserverTracker(selector, fn, observationTarget, timeout)
-    const observer = new MutationObserver((mutations, mutationObserver) => {
-
-        // TBD: should an existing selector yield an immediate invocation and not set up an observer?
-
-        // Search mutations where nodes were added for a node matching the selector
-        const foundTarget = mutations.flatMap(
-            mutation => Array.from(mutation.addedNodes))
-            .find(node => node.matches(selector));
-
-        if (foundTarget) {
-            try {
-                console.debug(`whenElementAppears: observer of ${selector} invoking callback`);
-                fn(foundTarget);
-                tracker.disconnect();
-                mutationObserver.disconnect();
-                return;
-            } catch (error) {
-                console.error('whenElementAppears: unexpected error...', error);
-            }
-        } else {
-            // TBD: I think I need to add handling in case disconnectAfterDetection is false
-
-            // For any new nodes with a shadowRoot, set up a new observer targeting the shadowRoot
-            for (node of mutations.flatMap(mutation => Array.from(mutation.addedNodes))) {
-                console.log(`ShadowRoot check on: ${node} : ${node.shadowRoot}`)
-                if (node.shadowRoot) {
-                    const shadowrootTracker = whenElementAppears(selector, fn, node.shadowRoot, timeout);
-                    tracker.shadowrootTrackers.push(shadowrootTracker);
-                }
-            }
-        }
-    })
-    observer.observe(observationTarget, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-    })
-    tracker.observer = observer
-    if (timeout) {
-        setTimeout(() => {
-            observer.disconnect();
-            console.log("Mutation Observer disconnected due to timeout");
-            tracker.disconnected = true;
-        }, timeout);
-    }
-
-    // Recursively check for shadowRoots and set up observers for them
-    const checkShadowRoots = (node) => {
-        if (node.shadowRoot) {
-            whenElementAppears(selector, fn, node.shadowRoot, timeout);
-        }
-        node.childNodes.forEach(child => checkShadowRoots(child));
-    };
-    checkShadowRoots(observationTarget);
-
-    return tracker
-};
-
-module.exports = whenElementAppears;
-
-
 class ObserverTracker {
     constructor(selector, fn, observationTarget, timeout) {
         this.created = new Date().getUTCMilliseconds()
@@ -86,11 +22,89 @@ class ObserverTracker {
         this.timeout = timeout
         this.disconnected = false
         this.shadowrootTrackers = []
+
+        // TBD: should an existing selector yield an immediate invocation and not set up an observer?
+
+        this.observer = new MutationObserver(async (mutations, mutationObserver) => {
+            await this.handleMutations(mutations)
+            logMutations(mutations)
+        })
+        this.observer.observe(observationTarget, {
+            childList: true,
+            subtree: true,
+            attributes: false,
+        })
+
+        if (timeout) {
+            setTimeout(() => {
+                this.disconnect();
+                console.log("Mutation Observer disconnected due to timeout");
+            }, timeout);
+        }
+
+        this.checkShadowRoots(observationTarget);
     }
+
+    // Recursively check for shadowRoots and set up observers for them
+    checkShadowRoots(node) {
+        if (node.shadowRoot) {
+            const shadowRootTracker = whenElementAppears(this.selector, this.fn, node.shadowRoot, this.timeout);
+            this.shadowrootTrackers.push(shadowRootTracker);
+        }
+        node.childNodes.forEach(child => this.checkShadowRoots(child));
+    }
+
     disconnect() {
-        this.shadowrootTrackers.forEach(st => st.disconnect());
         this.observer?.disconnect()
+        this.shadowrootTrackers.forEach(st => st.disconnect());
+        this.shadowrootTrackers = []
         this.disconnected = true
         this.observer = null
     }
+
+    async handleMutations(mutations) {
+        // Search mutations where nodes were added for a node matching the selector
+        const foundTarget = mutations.flatMap(
+            mutation => Array.from(mutation.addedNodes))
+            .find(node => node.matches(this.selector));
+
+        if (foundTarget) {
+            try {
+                console.debug(`whenElementAppears: observer of ${this.selector} invoking callback`);
+                this.fn(foundTarget);
+                this.disconnect();
+                return;
+            } catch (error) {
+                // TBD: provide users with an error callback instead?
+                console.error('whenElementAppears: unexpected error...', error);
+            }
+        } else {
+            // TBD: I think I need to add handling in case disconnectAfterDetection is false
+            mutations.forEach(mutation => {
+            // For any new nodes with a shadowRoot, set up a new observer targeting the shadowRoot
+                Array.from(mutation.addedNodes).concat(Array.from(mutation.removedNodes)).forEach(node => {
+                    if (node.shadowRoot) { // check if mode is open?
+                        const shadowrootTracker = whenElementAppears(this.selector, this.fn, node.shadowRoot, this.timeout);
+                        this.shadowrootTrackers.push(shadowrootTracker);
+                    }
+                });
+
+                // Check if the mutation target itself has a new shadow root
+                if (mutation.target.shadowRoot && !this.shadowrootTrackers.some(tracker => tracker.observationTarget === mutation.target.shadowRoot)) {
+                    const shadowrootTracker = whenElementAppears(this.selector, this.fn, mutation.target.shadowRoot, this.timeout);
+                    this.shadowrootTrackers.push(shadowrootTracker);
+                }
+
+            });
+
+        }
+    }
 }
+
+
+function whenElementAppears(selector, fn, observationTarget = document, timeout = 0) {
+    console.log(`whenElementAppears(${selector}, ${fn}, ${observationTarget}, ${timeout})`)
+    return new ObserverTracker(selector, fn, observationTarget, timeout)
+};
+
+module.exports = whenElementAppears;
